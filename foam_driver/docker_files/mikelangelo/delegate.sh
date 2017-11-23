f762b0319b361707b4a2d0b36db73a8a3cc19787
Miha Pleško <miha.plesko@xlab.si>
2017-11-23 15:31:43 +0100

    Support any number of OSv workers
    With this commit we implement support for running arbitrary
    number of OpenFOAM OSv workers.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- foam_driver/docker_files/mikelangelo/delegate.sh
+++ foam_driver/docker_files/mikelangelo/delegate.sh
@@ -13,12 +13,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# Usage: ./delegate.sh 4
+#
+
 set -o errexit
 set -o nounset
 set -o pipefail
 set -o errtrace
 
-echo "Mounting NFS directory to: /case"
-
-mkdir -p /case
-mount nfs.default:/case /case -o nolock
+parallelisation=${1:-2}
+workersList=$(curl --silent http://${KUBERNETES_URL}/api/v1/namespaces/default/pods?labelSelector=case%3Dopenfoam-worker | jq .items[].status.podIP)
+workersList=$(echo ${workersList} | sed s/"\""/""/g | sed s/" "/","/g)
+echo "Found workers: ${workersList}"
+echo " -> parallelisation: ${parallelisation}"
+curl -X PUT -d command="/usr/bin/mpirun -n ${parallelisation} -H ${workersList} --allow-run-as-root /usr/bin/simpleFoam.so -parallel -case /case" openfoam-worker.default:8000/app/
